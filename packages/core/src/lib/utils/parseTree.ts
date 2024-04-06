a67c1f8f56351b8aeddb940b5c4fd8f0a3eeb083
Denys Vuika <denys.vuika@gmail.com>
2024-04-06 11:00:10 -0400

    feat: add cloneTree utility (#27)

---
+++ packages/core/src/lib/utils/parseTree.ts
@@ -0,0 +1,33 @@
+import { EditorQueryMethods } from '../editor';
+import { cloneTree } from './cloneTree';
+import { fromEntries } from './fromEntries';
+import { QueryCallbacksFor } from './useMethods';
+
+export const parseTree = (
+  template: string | { rootNodeId: string; nodes: any },
+  query: QueryCallbacksFor<typeof EditorQueryMethods>
+): any => {
+  const data =
+    typeof template === 'string'
+      ? (JSON.parse(template) as { rootNodeId: string; nodes: any })
+      : template;
+
+  const newNodes = data.nodes;
+
+  const nodePairs = Object.keys(newNodes).map((id) => {
+    const nodeId = id;
+
+    return [
+      nodeId,
+      query.parseSerializedNode(newNodes[id], (node: any) => (node.id = nodeId))
+    ];
+  });
+
+  const tree = {
+    rootNodeId: data.rootNodeId,
+    nodes: fromEntries(nodePairs)
+  };
+  const result = cloneTree(tree, query);
+  console.log(result);
+  return result;
+};
