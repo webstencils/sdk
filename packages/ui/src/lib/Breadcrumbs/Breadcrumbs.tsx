d6c5b3459d2cc67bf4bf49838616686fb146e8e0
Denys Vuika <denys.vuika@gmail.com>
2024-04-08 13:01:44 -0400

    feat(ui): component breadcrumbs

---
+++ packages/ui/src/lib/Breadcrumbs/Breadcrumbs.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import MuiBreadcrumbs from '@mui/material/Breadcrumbs';
+import Typography from '@mui/material/Typography';
+import Link from '@mui/material/Link';
+import { useEditor } from '@webstencils/core';
+
+export function Breadcrumbs() {
+  const { actions, selectedAncestorNodes } = useEditor((_, query) => {
+    const currentlySelectedNodeId = query.getEvent('selected').first();
+
+    return {
+      selected: currentlySelectedNodeId,
+      selectedAncestorNodes: currentlySelectedNodeId
+        ? [
+            query.node(currentlySelectedNodeId).get(),
+            ...query
+              .node(currentlySelectedNodeId)
+              .ancestors(true)
+              .map((id) => query.node(id).get())
+          ].reverse()
+        : []
+    };
+  });
+
+  return (
+    <div role="presentation" onClick={(e) => e.preventDefault()}>
+      {selectedAncestorNodes.length > 0 ? (
+        <MuiBreadcrumbs sx={{ p: '4px' }}>
+          {selectedAncestorNodes.map((node, index, arr) => {
+            const displayName =
+              node.data.custom.displayName || node.data.displayName;
+            return index === arr.length - 1 ? (
+              <Typography color="text.primary">{displayName}</Typography>
+            ) : (
+              <Link
+                key={node.id}
+                underline="hover"
+                color="inherit"
+                href="/"
+                onClick={() => {
+                  if (index + 1 < selectedAncestorNodes.length) {
+                    actions.selectNode(node.id);
+                  }
+                }}
+              >
+                {displayName}
+              </Link>
+            );
+          })}
+        </MuiBreadcrumbs>
+      ) : null}
+    </div>
+  );
+}
